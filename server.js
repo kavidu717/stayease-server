16497fbce0ba8450d4a9a3c6f193f59ed748bc07
kavidu717 <dushmanthakavidu143@gmail.com>
2026-04-08 14:18:09 +0530

    connet the mongo db connection string

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
+dotenv.config()
 
 
 const app = express()
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('kavidu!')
 })
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+       console.log('Connected to MongoDB')
+})
+  
 
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000`)
