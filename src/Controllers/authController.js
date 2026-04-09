388bfe1e57237eae54a300986380d17b81a2aee3
kavidu717 <dushmanthakavidu143@gmail.com>
2026-04-09 20:42:38 +0530

    create login function with jwt

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -1,4 +1,5 @@
-
+import bcrypt from "bcrypt"
+import Jwt from "jsonwebtoken"
 
 import {User} from "../Models/userModels.js"
 
@@ -22,6 +23,11 @@ export const register=async(req,res)=>{
         .json({message:"User already exists"})
      }
 
+     //password hashing
+      
+     const hashPassword = await bcrypt.hash(password,10)
+
+     req.body.password=hashPassword
      // after that we can refistr the user
 
      const user = new User(req.body)
@@ -43,3 +49,66 @@ export const register=async(req,res)=>{
 
 }
 
+export const login=async(req,res)=>{
+  try{
+
+     const {email,password}=req.body;
+
+     if(!email || !password){
+        return res.status(400).json({
+          message:"Email and password are required"
+        })
+     }
+
+     const user=await User.findOne({email})
+
+     if(!user){
+        return res
+        .status(400)
+        .json({
+          message:"User not found"
+        })
+     }
+
+     const isMatch=await bcrypt.compare(password,user.password)
+
+     if(!isMatch){
+        return res
+        .status(400)
+        .json({
+          message:"Password is incorrect"
+        })
+     }
+
+   
+
+     const token=Jwt.sign(
+        {
+            id:user._id,
+
+        },
+        process.env.JWT_SECRET_KEY,
+        {expiresIn:"1d"}
+     )
+
+     return res
+     .status(200)
+     .json({
+
+       token,
+
+        message:"Login successful",
+        user,
+       
+     })
+    
+     
+
+  }
+  catch(err){
+     console.log(err)
+     return res.status(500).json({
+        message: err.message || "Something went wrong"
+     })
+  }
+}
--- src/Models/userModels.js
+++ src/Models/userModels.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 6,
-        maxlength: 20
+        maxlength: 200
     },
 
     stripeAccountId: {
